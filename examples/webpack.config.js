230345194386ca5fb709a0fab17737dce57600f1
Mykhailo Aliinyk <m.aliynik@gmail.com>
2021-11-12 15:07:06 +0200

    Fixed not found worker.worker.js file

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
     examples: path.resolve(__dirname, 'src/index.tsx')
   },
   output: {
-    path: path.resolve(__dirname, 'data')
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
@@ -21,6 +22,11 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js']
   },
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        'node_modules/@tynik/web-workers/dist/worker.worker.js'
+      ]
+    }),
     new HtmlWebpackPlugin({
       template: 'src/index.ejs'
     })
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    index: path.resolve(__dirname, 'src/index.ts'),
+    index: path.resolve(__dirname, 'src/index.ts')
   },
   output: {
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
@@ -19,5 +20,9 @@ module.exports = {
   devtool: 'source-map',
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
+  },
+  externals: {
+    'react': 'commonjs react',
+    'react-dom': 'commonjs react-dom'
   }
 }
